6e3053f9d1a4d78cb9f91d0f45ac83887f1978cc
schmitz1193 <schmitz1193@gmail.com>
2015-10-29 15:13:23 -0500

    Final French Translate

--- french.js
+++ french.js
@@ -1,23 +1,30 @@
 
 
-//IIFE for Translate
+//IIFE for Translate to French
 
-var Translate = (function(inToFrench) {
-		var french = {"merry":"joyeux", 
+var Translate = function(translate) {
+	var lexicon = {
+					"merry":"joyeux", 
 					"christmas":"noël", 
 					"and":"et", 
 					"happy":"content", 
 					"new":"neuf", 
-					"year":"an"};
+					"year":"an"
+					};
 
-	inToFrench.getFrenchwords: function() {
-		return frenchWords;
-	},
-	inToFrench.translateToFrench: function(text) {
+	translate.toGreek = function(userMessage) {
+			var messageArray = userMessage.toLowerCase().split(" ");
+			var translatedArray = messageArray.map(function(word) {
+					if (word in lexicon === false) {
+						return word;
+					} else {return lexicon[word]};
+				});
+				var translatedMessage = translatedArray.join(" ");
+				return translatedMessage;
+			}
+	return translate;
 
-	}
-
-	return inToFrench;
+}(Translate);
 
 
-})(Translate);
+
