fb7f426a5e76b3759ad97ada9c8c723545a9f239
schmitz1193 <schmitz1193@gmail.com>
2015-10-29 14:59:30 -0500

    french.js

---
+++ french.js
@@ -0,0 +1,23 @@
+
+
+//IIFE for Translate
+
+var Translate = (function(inToFrench) {
+		var french = {"merry":"joyeux", 
+					"christmas":"noël", 
+					"and":"et", 
+					"happy":"content", 
+					"new":"neuf", 
+					"year":"an"};
+
+	inToFrench.getFrenchwords: function() {
+		return frenchWords;
+	},
+	inToFrench.translateToFrench: function(text) {
+
+	}
+
+	return inToFrench;
+
+
+})(Translate);
